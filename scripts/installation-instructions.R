e32a5afdf1598f1fc8c53c3109624a801253df1a
Katrien Antonio <u0043788@kuleuven.be>
2021-01-12 15:13:17 +0100

    upload installation instructions

---
+++ scripts/installation-instructions.R
@@ -0,0 +1,11 @@
+packages <- c("tidyverse", "here", "rstudioapi", "rpart", "rpart.plot", "vip", "pdp", "ipred", "ranger", "gbm", "xgboost", "rstudioapi", "gganimate", "transformr", "Ckmeans.1d.dp", "classInt")
+
+new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+
+if(sum(!(packages %in% installed.packages()[, "Package"]))) {
+  stop(paste('The following required packages are not installed:\n', 
+             paste(packages[which(!(packages %in% installed.packages()[, "Package"]))], collapse = ', ')));
+} else {
+  message("Everything is set up correctly. You are ready to go.")
+}
